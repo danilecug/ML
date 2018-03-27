d19ff70dfb200c163848ee03c16cadfd1eb2e1f4
Zhi Zhong <danilecug@gmail.com>
2018-03-27 00:37:52 -0500

    modify

--- test1.py
+++ test1.py
@@ -1,5 +1,8 @@
 
 
+
+
+
 import pandas as pd
 import numpy as np
 
@@ -7,4 +10,4 @@ class Test(object):
     def __init__(self):
 	super(Test).__init__(self)
     def _getProperties(self):
-	print('This is get the properties')
+	print('This is get the properties') 
