94c15d2265d3054b1eb4c9830bb2e72614e2129b
Zhi Zhong <danilecug@gmail.com>
2018-03-27 00:34:07 -0500

    first python code

---
+++ test1.py
@@ -0,0 +1,10 @@
+
+
+import pandas as pd
+import numpy as np
+
+class Test(object):
+    def __init__(self):
+	super(Test).__init__(self)
+    def _getProperties(self):
+	print('This is get the properties')
