8c9fecc0e1cc21c8f7e9b02b526ea3b3589dfc20
Zhi Zhong <danilecug@gmail.com>
2018-03-27 21:49:10 -0500

    new

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 
 
 
+
 print('This is my first python code wrote by linux')
+print('This is another additional python code for the linux')
+
+
+print('We need to learn the molecure simulation stuff')
+ 
+
+
+
 
 
 
