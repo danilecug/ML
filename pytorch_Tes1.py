45ecc18ae5df7486334886f3a9c93a43d1cf41e0
Zhi Zhong <danilecug@gmail.com>
2018-03-27 22:12:27 -0500

    pytorch_Test1_2

--- pytorch_Tes1.py
+++ pytorch_Tes1.py
@@ -12,9 +12,22 @@ torch.manual_seed(1)
 ## LSTM cell (input->3, output->3)
 lstm = nn.LSTM(3,3)
 inputs = [autograd.Variable(torch.randn((1,3))) for _ in range(5)]
+print(inputs)
+
+
+
 
 hidden = (autograd.Variable(torch.randn((1,1,3)),
 	autograd.Variable(torch.randn((1,1,3))))
+
+
+
+
+
+
+
+print(hidden) 
+
 
 for i in inputs:
     out, hidden = lstm(i.view(1,1,-1), hidden)
