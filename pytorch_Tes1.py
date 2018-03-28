4d15e2be75dd5715a320bd2b1e09c0101df9584e
Zhi Zhong <danilecug@gmail.com>
2018-03-27 22:08:10 -0500

    pytorch_Test1

---
+++ pytorch_Tes1.py
@@ -0,0 +1,23 @@
+#Author: Zhi Zhong
+
+
+import torch 
+import torch.autograd as autograd
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+torch.manual_seed(1)
+
+## LSTM cell (input->3, output->3)
+lstm = nn.LSTM(3,3)
+inputs = [autograd.Variable(torch.randn((1,3))) for _ in range(5)]
+
+hidden = (autograd.Variable(torch.randn((1,1,3)),
+	autograd.Variable(torch.randn((1,1,3))))
+
+for i in inputs:
+    out, hidden = lstm(i.view(1,1,-1), hidden)
+    print(out)
+    print(hidden)
+
